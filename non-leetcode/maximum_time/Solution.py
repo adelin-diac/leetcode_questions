0c0ace0badc73f039fc625453a7040811bf95e93
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2023-05-15 16:57:58 +0100

    update docs cntd

--- non-leetcode/maximum_time/Solution.py
+++ non-leetcode/maximum_time/Solution.py
@@ -1,39 +1,39 @@
 import sys
 
 def solution(A):
-  # if(A.count("?") == 4):
-  #   return "23:59"
+  if(A.count("?") == 4):
+    return "23:59"
 
-  # A = A.split(":")
-  # if A[0].count("?") == 2:
-  #   A[0] = "23"
-  # elif A[0].count("?") == 0:
-  #   pass
-  # else:
-  #   pos1 = A[0].find("?")
-  #   match pos1:
-  #     case 0:
-  #       if int(A[0][1]) < 4:
-  #         A[0] = A[0].replace("?", "2")
-  #       else:
-  #         A[0] = A[0].replace("?", "1")
-  #     case 1:
-  #       if int(A[0][0]) == 2:
-  #         A[0] = A[0].replace("?", "3")
-  #       else:
-  #         A[0] = A[0].replace("?", "9")
+  A = A.split(":")
+  if A[0].count("?") == 2:
+    A[0] = "23"
+  elif A[0].count("?") == 0:
+    pass
+  else:
+    pos1 = A[0].find("?")
+    match pos1:
+      case 0:
+        if int(A[0][1]) < 4:
+          A[0] = A[0].replace("?", "2")
+        else:
+          A[0] = A[0].replace("?", "1")
+      case 1:
+        if int(A[0][0]) == 2:
+          A[0] = A[0].replace("?", "3")
+        else:
+          A[0] = A[0].replace("?", "9")
 
-  # if A[1].count("?") == 2:
-  #   A[1] = "59"
-  # elif A[1].count("?") == 0:
-  #   pass
-  # else:
-  #   pos1 = A[1].find("?")
-  #   match pos1:
-  #     case 0:
-  #       A[1] = A[1].replace("?", "5")
-  #     case 1:
-  #       A[1] = A[1].replace("?", "9")
+  if A[1].count("?") == 2:
+    A[1] = "59"
+  elif A[1].count("?") == 0:
+    pass
+  else:
+    pos1 = A[1].find("?")
+    match pos1:
+      case 0:
+        A[1] = A[1].replace("?", "5")
+      case 1:
+        A[1] = A[1].replace("?", "9")
 
   return A[0] + ":" + A[1]
 

ec772a34904e72c7a8cbaff1f96a872fa1c3b9e0
adelin-diac <adelin334@gmail.com>
2024-08-09 18:16:23 +0300

    grafting

--- medium/group_anagrams/Solution.py
+++ medium/group_anagrams/Solution.py
@@ -1,7 +1,25 @@
+from typing import List
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         grouped = {}
+        ret = []
         for item in strs:
-            grouped[item.sort()].append(item)
+            item_sorted = ''.join(sorted(item))
+
+            if item_sorted in grouped:
+              grouped[item_sorted].append(item)
+            else:
+                grouped[item_sorted] = [item]
         
-        return grouped
+        for key in grouped:
+            ret.append(grouped[key])
+
+        return ret
+
+
+s = Solution()
+print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(s.groupAnagrams([""]))
+print(s.groupAnagrams(["a"]))
+print(s.groupAnagrams([]))
